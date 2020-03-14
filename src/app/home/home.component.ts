aa12e003276a02d9065853a305644806391d07d4
githumbi <githumbi74@gmail.com>
2020-03-14 15:57:41 +0300

    notification of username

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
+
 
 import { UserService, AuthenticationService } from '../_services';
 
@@ -16,18 +16,8 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        // this.loadAllUsers();
+  
     }
 
-    // deleteUser(id: number) {
-    //     this.userService.delete(id)
-    //         .pipe(first())
-    //         .subscribe(() => this.loadAllUsers());
-    // }
 
-    // private loadAllUsers() {
-    //     this.userService.getAll()
-    //         .pipe(first())
-    //         .subscribe(users => this.users = users);
-    // }
 }
